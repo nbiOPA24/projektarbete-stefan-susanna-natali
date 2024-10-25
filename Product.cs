b5a49ab3822a3ced6f3a8e6041b67a87ad092a83
stefan lundberg <bjorn.stefan.lundberg@gmail.com>
2024-10-25 11:39:30 +0200

    Nu skrivs produkten ut enbart 1 gång. AddProduct är fungerande

--- Product.cs
+++ Product.cs
@@ -1,28 +1,25 @@
-using System.Runtime.Intrinsics.Arm;
-using System.Runtime.Serialization.Formatters;
-
+// Klass för en produkt med div. egenskaper.
 public class Product
 {   
     public enum ProductType // förrätt, varmrätt, dessert 
     {
         Food,
-        Drinks
-        
-        
-        
+        Drinks    
     }
+
+//  int[] array = new int[2];
+//  string[] ProductType = ["food", "Drinks"];
     public enum VatRate //momssatser för produkter 
     {
         _12,
         _25
-        
     }
 
     public string Name {get ; set ;}
     public double Price {get ; set ;}
     //public double VAT {get; set;}
-    public ProductType MenuItem {get ; set ;}
-    public VatRate VatItem {get ; set ;} // bättre namn?
+    public ProductType MenuItem {get ; set ;} // typ av produkt som relaterar till vilken moms som gäller för produkten
+    public VatRate VatItem {get ; set ;} // momssats
 
     public Product (string name, double price, ProductType menuItem, VatRate vatItem)
     {
@@ -30,24 +27,24 @@ public class Product
         Price = price;
         MenuItem = menuItem;
         VatItem = vatItem;
-        
-    }
-
-    
+    }    
 }
 public static class ProductHandler
 {
     public static List<Product> productList {get ; set ;} = new();
 
-    public static List<Product> PrintProduct (List<Product> productList) // varför inte void tex?
+    public static void PrintProduct () // varför inte void tex?
+    //Om du bara ska skriva ut produkter är void tillräckligt. 
+    //Om du vill att metoden också ska "passa vidare" listan kan List<Product> som returtyp vara ett bra alternativ.
     {
         foreach (Product p in productList)
         {
             
             Console.WriteLine(productList.Count());
             Console.WriteLine(p.MenuItem +": "+ p.Name + " - " + p.Price + " kr " + p.VatItem);
+            
         }
-        return productList;
+
     }
 
     public static void AddProduct(Product product) //  If food == vatRate._12
@@ -58,8 +55,10 @@ public static class ProductHandler
         
         var typearray = Enum.GetValues(typeof(Product.ProductType)); // gör om producttype till array
         var vatarray = Enum.GetValues(typeof(Product.VatRate)); // gör om vatItem till array 
+        
         int input = int.Parse(Console.ReadLine()); 
-        Product.ProductType selectedItemType = (Product.ProductType)typearray.GetValue(input);
+        
+        Product.ProductType selectedItemType = (Product.ProductType)typearray.GetValue(input); // 
         Product.VatRate selectedVatType = (Product.VatRate)vatarray.GetValue(input);
         Console.Write("Produktens namn: ");
         string? name = Console.ReadLine();
@@ -67,9 +66,7 @@ public static class ProductHandler
         double price = double.Parse(Console.ReadLine());
         Product newProduct = new(name, price, selectedItemType, selectedVatType);
         productList.Add(newProduct);                
-        PrintProduct(productList);
-
-        
+        PrintProduct();    
     }
 
     public static void PrintProductType()
