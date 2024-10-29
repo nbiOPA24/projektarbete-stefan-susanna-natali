748d47386c5ad8e8bd1ad29d48d6adb1140ef944
stefan lundberg <bjorn.stefan.lundberg@gmail.com>
2024-10-29 10:49:58 +0100

    Add Report.cs and Product.cs to continue developing separately from project

--- Report.cs
+++ Report.cs
@@ -1,52 +1,60 @@
-/*public class Report
+public class Report
 {
-    public enum ReportType
+    public enum ReportCategory //ändrade från 'type' till 'category'
     {
         TotalSales,
         WeeklySales,
         DailySales,
         PrintReceipt
     }
-}
+
      public int ReportNumber {get; set;}
      public DateTime Date {get; set;}
-     public ReportType Type {get; set;} // TODO ska den heta type? 
+     public ReportCategory Type {get; set;} // Ändrade från 'type' till 'Category'
 
-     public List<Sale> Sales {get; set;} = new(); 
+     public List<Sales> Sales {get; set;} = new(); 
 
      public void AddSale(decimal amount, DateTime date)
      {
-         Sales.Add(new Sale(amount, date));
+         Sales.Add(new Sales(amount, date));
      }
-*/
+}
 
- /*public class Sale // för att hantera individuella 'sales' inom 'Report'-rapporter.
-                    //TODO gör en egen flik för Sale-klassen?
+
+ public class Sales // för att hantera individuella 'sales' inom 'Report'-rapporter.
+                    //TODO gör en egen flik för Sales-klassen?
+                    //TODO länka Sales och produkt-lista
  {
-     public decimal Amount {get; set;} //"decimal" för att hantera monetära beräkningar
+     public decimal TotalAmount => Quantity * Product.Price; //kalkylerar total försäljning av 'Product'
+     public Product Product {get; set;} //länka Sales till Product som säljs
      public DateTime Date {get; set;} //TODO kanske fler egenskaper än summa + tid/datum?
+//   public int SalesId {get; set;} //?? ett sätt att identifiera varje unik 'sale' tillfälle, dvs 'Table'
 
-     public Sale(decimal amount, DateTime date)
+     public int Quantity {get; set;} // 'samlar' antal unika sales-tillfällen
+
+
+     public Sales(Product product, int quantity, DateTime date)
      {
-         Amount = amount;
+         Product = product;
          Date = date;
+         Quantity = quantity;
      }
- }*/
-/*public static class ReportHandler
+ }
+public static class ReportHandler
 {
     public static List<Report> SalesList {get; set;}  =  new(); 
 
-    public static decimal ReportGenerator(Report.ReportType reportType) //generarar sales-rapporter av de slag vi vill ha
+    public static decimal ReportGenerator(Report.ReportCategory reportCategory) //generarar sales-rapporter av de slag vi vill ha
     {
-        if (reportType == reportType.Total)
+        if (reportCategory == reportCategory.Total)
         {
             return TotalSales();
         }
-        else if (reportType == reportType.Weekly)
+        else if (reportCategory == reportCategory.Weekly)
         {
             return WeeklySales();
         }
-        else if (reportType == reportType.Daily)
+        else if (reportCategory == reportCategory.Daily)
         {
             return DailySales();
         }
@@ -60,7 +68,7 @@
     public static decimal DailySales(){}
     public static decimal PrintReport(){}
     
-}*/
+}
 /* public static string PrintReport(Report report)
 {
     var report = 
