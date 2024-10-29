6e9d9882305210a665c8fc5d645818f9d1441704
stefan lundberg <bjorn.stefan.lundberg@gmail.com>
2024-10-29 10:35:38 +0100

    List Tablemap created and testing new class Tablemap for future functionality

--- Table.cs
+++ Table.cs
@@ -1,19 +1,20 @@
 public class Table
 {
+    int XPosition { get; set; }
+    int YPosition { get; set; }
     int Number {get; set;}
-    public enum ReportCategory // Tableposition?
-    {
-        X,
-        Y,
-        Z,
-    }
     int Size {get;set;}
 }
-public static class TableHandler
-{
-    //2D Bordsarray? 
+public class TableHandler
+{ 
+    public Table Tablemap {get; set;} = new Table();
     public static List<Table> listOfTables = new();
     public static void AddTable(){}
     public static void RemoveTable(){}
     public static void ChangeTable(){}
+}
+
+public class Tablemap
+{
+
 }
