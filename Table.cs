2a4952ac4bc557681bd3b15656862ce4c07b8b58
stefan lundberg <bjorn.stefan.lundberg@gmail.com>
2024-10-31 11:22:13 +0100

    Testar brancha

--- Table.cs
+++ Table.cs
@@ -186,7 +186,7 @@ public class TableMap : Table//klass för en bordskarta, testar arv
                 default: isValidKey = false; break;
             }
 
-            // lägga till typ "space" för att låsa?
+            // lägga till typ "space" för att låsa och esc för avbryta?
             if (isValidKey &&
                 newX >= 0 && newX < width &&
                 newY >= 0 && newY < height)
