86e072f66b7ca65944f6a2b5b4824197631f2ceb
Susanna Rokka <susannaajrokka@gmail.com>
2024-11-12 19:04:42 +0100

    Integrerat meny-logik för *Report' i UserIF. Kommenterat kod

--- Report.cs
+++ Report.cs
@@ -3,7 +3,11 @@ using System.Security.Cryptography;
 
 public class Report
 {
+<<<<<<< Updated upstream
     public enum ReportCategory //ändrade från 'type' till 'category'
+=======
+    public enum ReportCategory
+>>>>>>> Stashed changes
     {
         TotalSales,
         WeeklySales,
@@ -11,7 +15,12 @@ public class Report
         PrintReceipt
     }
 
+<<<<<<< Updated upstream
     public static bool GetDate(string prompt, out DateTime date) //TODO fixa så att den tar emot datum i formatet YYYY-MM-DD
+=======
+//tar emot datum-input i formatet YYYY-MM-DD för rapporter. TODO ta 'dagens datum' för dailyrapport
+    public static bool GetDate(string prompt, out DateTime date)
+>>>>>>> Stashed changes
     {
         Console.WriteLine(prompt);
         string? dateInput = Console.ReadLine();
@@ -22,7 +31,11 @@ public class Report
 
      public int ReportNumber {get; set;}
      public DateTime Date {get; set;}
+<<<<<<< Updated upstream
      public ReportCategory Category {get; set;} // Ändrade från 'type' till 'Category'
+=======
+     public ReportCategory Category {get; set;} 
+>>>>>>> Stashed changes
 
      public List<Sales> Sales {get; set;} = new(); 
 
@@ -52,11 +65,16 @@ public class Report
      }
  }
 
+<<<<<<< Updated upstream
 
+=======
+//här hanteras Rapport-val med meny-logik via user input (huvudmenyn från UserInterFace)
+>>>>>>> Stashed changes
 public static class ReportHandler
 {
     public static List<Report> SalesList {get; set;}  =  new(); 
 
+<<<<<<< Updated upstream
     public static decimal ReportGenerator(Report.ReportCategory reportCategory, DateTime startDate, DateTime endDate) //generarar sales-rapporter av de slag vi vill ha
                                                                                 //lade till manuell DateTime-filtrering for now baserat på user input
                                                                                 //TODO integrera maunell datum-input till Table??
@@ -120,3 +138,122 @@ public static class ReportHandler
 {
     var report = 
 } */
+=======
+    public static void ReportMenu()
+    {
+            
+                    Console.WriteLine("Var god välj vilken Rapport du vill generera: ");
+                    Console.WriteLine("Tryck 1. För TotalSales: ");
+                    Console.WriteLine("Tryck 2. För WeeklySales: ");
+                    Console.WriteLine("Tryck 3. För DailySales: ");
+                    Console.WriteLine("Tryck Q. För att avsluta till huvudmenyn: ");
+
+                    string? reportChoice = Console.ReadLine();
+                    Report.ReportCategory reportCategory = reportChoice 
+                    switch 
+                    {
+                        "1" => Report.ReportCategory.TotalSales,   //TODO ändra denna meny - dra index från enum i 'Report' ist?
+                        "2" => Report.ReportCategory.WeeklySales,
+                        "3" => Report.ReportCategory.DailySales,
+                        "4" => Report.ReportCategory.PrintReceipt,
+                      "Q" or  _   => Report.ReportCategory.PrintReceipt,
+                    };
+                    if (reportCategory == Report.ReportCategory.PrintReceipt)
+                        {
+                            Console.WriteLine("Tillbaka till huvudmenyn. Välkommen åter!");
+                            return;
+                        }
+                    //här tar man in manuell input för datum-spann för vald rapport
+                    //TODO att kunna välja veckonummer, eller ur en kalender
+                    if (!Report.GetDate($"Ange startdatum för rapporten (YYYY-MM-DD):", out DateTime startDate) || //Done! justerat för dynamik från user-input
+                        !Report.GetDate($"Ange slutdatum för rapporten (YYYY-MM-DD): ", out DateTime endDate))     // TODO Lägg in exception för inkorrekt datum-format enligt mall YYYY-MM-DD
+                    {
+                        Console.WriteLine("Ogiltig input. Försök igen (YYYY-MM-DD).");
+                        return;
+                    }
+            
+            
+                
+            //Här visas Rapport-data i konsollen
+            //TODO lägga till fler rapport-kategorier samt kanske kolumner för rapport?
+
+            decimal reportTotal = ReportGenerator(reportCategory, startDate, endDate);
+            Console.WriteLine($"Total försäljning för {reportCategory} är: {reportTotal: C}");
+    }
+
+    
+
+    public static decimal ReportGenerator(Report.ReportCategory reportCategory, DateTime startDate, DateTime endDate) //generarar sales-rapporter av de slag vi vill ha
+                                                                                //lade till manuell DateTime-filtrering for now baserat på user input
+                                                                                //TODO integrera maunell datum-input till Table??
+                                                                                //TODO tighta till logiken i R-generator, redundans?
+   {
+        return reportCategory switch
+            {
+                Report.ReportCategory.TotalSales => TotalSales(startDate, endDate),
+                Report.ReportCategory.WeeklySales => WeeklySales(startDate, endDate),
+                Report.ReportCategory.DailySales => DailySales(startDate, endDate),
+                _ => 0
+            };
+        }
+
+    //logik för hur rapporterna kompileras
+    public static decimal TotalSales(DateTime startDate, DateTime endDate)
+    {
+        return SalesList
+        .Where(report => report.Date >= startDate && report.Date <= endDate)//sorterar efter valt datum-spann
+        .Sum(report=> report.Sales.Sum(sale => sale.TotalAmount)); //ser till att få en rapport-kategori i taget att funka
+    } 
+    public static decimal WeeklySales(DateTime startDate, DateTime endDate)
+    {
+        return SalesList
+        .Where(report => report.Date >= startDate && report.Date <= endDate)
+        .SelectMany(report => report.Sales)
+        .GroupBy(sale=> CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(sale.Date, CalendarWeekRule.FirstDay, DayOfWeek.Monday))
+        .Sum(group=> group.Sum(sale => sale.TotalAmount));
+    }
+    public static decimal DailySales(DateTime startDate, DateTime endDate)
+    {
+        return SalesList
+        .Where(report => report.Date >= startDate && report.Date <= endDate)
+        .SelectMany(report => report.Sales)
+        .GroupBy(sale=> sale.Date.Date)
+        .Sum(group=> group.Sum(sale => sale.TotalAmount));
+    }
+
+    //här vill jag ha 'itemised' och grupperad produkt-försäljning
+    public static void ProductSalesReport (DateTime startDate, DateTime endDate) //visar Rapport på lista av produkter
+    {
+        var salesReport = SalesList
+            .Where(report => report.Date >= startDate && report.Date <= endDate)
+            .SelectMany(report => report.Sales) //tar sales-listan på alla rapporter
+            .GroupBy(sale => sale.Product.Name) //tar total sales/försäljning per product-namn??? eller?
+            .Select(group => new
+            {
+                ProductName = group.Key,
+                TotalQuantitySold = group.Sum(s => s.Quantity),
+                TotalSalesAmount = group.Sum(s => s.TotalAmount)  
+            })
+            .OrderByDescending(report => report.TotalSalesAmount);
+
+            Console.WriteLine("Försäljningsrapport: ");
+            foreach (var item in salesReport)
+            {
+                Console.WriteLine($"Produkt: {item.ProductName}, Total antal styck sålda: {item.TotalQuantitySold}, Total försäljning summa: {item.TotalSalesAmount:C}");
+            }
+    }
+   
+}
+    
+
+    
+
+ 
+    //public static decimal PrintReport(){}
+    
+
+/* public static string PrintReport(Report report) {}
+{
+    var report = 
+} */
+>>>>>>> Stashed changes
--- UserInterFace.cs
+++ UserInterFace.cs
@@ -22,6 +22,85 @@ public static class UserInterFace
         }
 
     }
+<<<<<<< Updated upstream
+=======
+
+    // Ev. TODO ev. välja nollbong eller funktion för detta? 
+    public static void Order(bool status, Product product, TableHandler tablehandler) //TODO kan den plussa istället för att lägga samma artikel på ny rad? 
+    {
+        Console.WriteLine();
+        ProductHandler.PrintProduct();
+        Console.WriteLine();
+
+        while (true)
+        {
+            Console.Write("Skriv in beställning, ange artikelnr. 'Q' för klar: ");
+            string? choice = Console.ReadLine().ToUpper();
+
+            if (int.TryParse(choice, out int number))
+            {
+                Product productsToAdd = ProductHandler.productList.Find(product => product.ProductNumber == number);
+                if (productsToAdd != null)
+                {
+                    orderList.Add(productsToAdd);
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Ogiltig input");
+                }
+            }
+            PrintOrderlist();
+            CountTotal();
+
+            if (choice == "Q")
+            {
+                Console.WriteLine("Betala (d)irekt eller lägga order till (b)ord?");
+                string paymentChoice = Console.ReadLine();
+                paymentChoice = UppercaseFirst(paymentChoice);
+                if (paymentChoice == "D")
+                {
+                    Payment();
+                    orderList.Clear();
+                    //TODO lägg i rapport-lista
+                    break;
+                }
+                else if (paymentChoice == "B")
+                {
+
+                    /*TableHandler tableHandler = new();
+                    TableHandler.ShowTables();
+                    TableHandler.OrderToTable(number, status, product, tableHandler, tablehandler);
+                    //TODO lägg i rapport-lista*/
+                    orderList.Clear();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltig input");
+
+                }
+
+            }
+
+        }
+    }
+
+    public static void CountTotal()
+    {
+        TableHandler tableHandler = new();
+        AmountToPay = 0; //Nollställ efter varje knapptryckning när man lägger på en ny artikel
+        foreach (Product p in tableHandler.tableProductList)
+        {
+            AmountToPay += p.Price; //p.Quantity *
+
+        }
+        Console.WriteLine("Summa att betala: " + AmountToPay);
+
+
+    }
+>>>>>>> Stashed changes
     public static void Payment()
     {
 
@@ -244,7 +323,86 @@ public static class UserInterFace
             }
         }
 
+<<<<<<< Updated upstream
 
+=======
+    }
+    public static void UserInterFaceStartMenu(Product product, TableHandler tableHandler, User user, int number, bool status, int size)
+    {
+        // Console.WriteLine("Välkommen!");
+        // UserHandler.PrintUser(user);
+        // Console.Write("Välj användare, ange ID-nummer: ");
+        TableHandler.GenerateTables();
+        UserChoice = 2401;//int.Parse(Console.ReadLine());
+        while (true)
+        {
+            Console.WriteLine("*****SUNAST-KASSASYSTEM*****");
+            Console.WriteLine("1. Ny Order");//Ny beställning");
+            Console.WriteLine("2. Hantera Order"); // hämta order på bord
+            Console.WriteLine("3. Se restaurangmeny");
+            Console.WriteLine("4. Bordshantering");
+            Console.WriteLine("5. Produktmeny");
+            Console.WriteLine("6. Personalmeny");
+            Console.WriteLine("7. Rapportmeny");
+            Console.WriteLine("8. Avsluta");
+
+            string? choice = Console.ReadLine().ToUpper();
+            switch (choice)
+            {
+                case "1":
+                    /*Order(status, product);*/
+                    break;
+                case "2":
+                    /*TableHandler.ShowOpenTables();
+                    tableHandler.HandleTableContents(number, tableHandler);*/
+                    break;
+                case "3":
+                    DisplayMenu();
+                    //if(admin)
+                    Console.WriteLine("1. Skapa meny");
+                    Console.WriteLine("2. Ändra meny");
+                    Console.Write("Q för att avsluta: ");
+                    string? menuchoice = Console.ReadLine().ToUpper();
+                    switch (menuchoice)
+                    {
+                        case "1":
+                            CreateMenuDescription();
+                            break;
+                        case "2":
+                            EditMenuDescription();
+                            break;
+                        case "Q":
+                            return;
+                    }
+                    break;
+                case "4":
+                    TableHandler.TableMenu(number, status, size, product);
+                    break;
+                case "5":
+                    ProductHandler.ProductStartMenu();
+                    break;
+                case "6":
+                    UserHandler.UserStartMenu(user);
+                    break;
+                case "7":
+                    ReportHandler.ReportMenu();
+                    break;
+                case "8":
+                    return;
+                case "Q":
+                    Back(choice);
+                    break;
+            }
+        }
+    }
+    public static bool Back(string input)
+    {
+        if (input == "Q")
+        {
+            Console.WriteLine("Tas tillbaka");
+            return true;
+        }
+>>>>>>> Stashed changes
 
 
     }
