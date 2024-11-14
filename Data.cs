4b7bf146f73dd3a6cb7d57ac298e194cdcdd3010
Susanna Rokka <susannaajrokka@gmail.com>
2024-11-14 11:10:44 +0100

    byte av filnamn för Datacontainer.cs

--- Data.cs
+++ Data.cs
@@ -1,7 +1,21 @@
+using System.Text.Json.Serialization;
+using System.Xml;
 using Newtonsoft.Json;
+<<<<<<< HEAD:Data.cs
 using System;
 using System.Collections.Generic;
 using System.IO;
+=======
+using System.IO;
+public class DataContainer
+{    //Lägg in listorna
+    public static List<User> userList = new();
+public void SaveOrderAsJson(string filePath)
+{
+    string json = JsonConvert.SerializeObject(userList);
+    File.WriteAllText(filePath, json);
+}
+>>>>>>> d4ef7b1 (merge med upstream main):DataContainer.cs
 
 public static class Data
 {    //Lägg in listorna
@@ -12,18 +26,36 @@ public static void SaveJson(string filePath)
     File.WriteAllText(filePath, json);
 }
 
-public static void LoadJson(string filePath)
-{
-    if (File.Exists(filePath))
+    public static void LoadJson(string filePath)
     {
-        string json = File.ReadAllText(filePath);
-        UserHandler.userList = JsonConvert.DeserializeObject<List<User>>(json);
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+
+            User user = JsonConvert.DeserializeObject<User>(json);
+            UserHandler.userList = new List<User> { user };
+        }
+        else 
+        {
+            Console.WriteLine("Inga objekt hittade!");
+        }
     }
-    else 
-    {
-        Console.WriteLine("Inga objekt hittade!");
-    }
+<<<<<<< HEAD:Data.cs
+=======
 }
+
+public static void SaveProductJson(List<Product>productList)
+{
+    string productJson = JsonConvert.SerializeObject(productList);
+    File.WriteAllText("product.json", productJson);
+}
+
+public static void SaveSalesJson(List<Sales> Sales)
+{
+    string salesJson = JsonConvert.SerializeObject(Sales);
+    File.WriteAllText("sales.json", salesJson);
+}
+>>>>>>> d4ef7b1 (merge med upstream main):DataContainer.cs
     // public static void SaveJson(string filePath, DataContainer dataContainer)
     // {
     //     try
